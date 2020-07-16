861cae84b1445fdc558229a431ceab346589b7f8
Sebastian Seijo <sebastiangseijo@gmail.com>
2020-07-17 00:55:20 +0200

    Add EmailData class. Remove local gmail variables add first run functionality. Add print information.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
 import smtplib
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
@@ -7,33 +7,58 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 
+from EmailData import EmailData
+
 load_dotenv()
-gmail_user = os.environ.get('gmail_user')
-gmail_password = os.environ.get('gmail_password')
-sent_from = gmail_user
-to = ['sebastiangseijo@gmail.com']
-subject = 'VRA STAKING UPDATE'
-body = f'VRA has change from '
-
-browser = webdriver.Chrome('./chromedriver')
-browser.get('https://verawallet.tv/stake-vra')
-timeout = 5
-try:
-    element_present = EC.presence_of_element_located((By.CLASS_NAME, 'staking-block'))
-    WebDriverWait(browser, timeout).until(element_present)
-    vra_element = browser.find_elements(By.CSS_SELECTOR, '.staking-block .staking-block-value')[2].text
-    vra_number = vra_element.replace(' VRA', '')
-    available_for_stake = (int(vra_number) > 50000)
-    print(available_for_stake)
-    if available_for_stake:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.ehlo()
-        server.login(gmail_user, gmail_password)
+first_run = None
 
 
-except TimeoutException:
-    print("Timed out waiting for page to load")
-except:
-    print('an error has occurred.')
+def main():
+    global first_run
+    vra_default_value = 0
+    browser = webdriver.Chrome('./chromedriver')
+    browser.get('https://verawallet.tv/stake-vra')
+    timeout = 5
+    try:
+        # Refresh web if it's not the first run
+        if first_run is not None:
+            browser.implicitly_wait(15)
+            browser.refresh()
+        print('Reading VRA Wallet Stake status')
+        email_data = EmailData()
+        # get staking block element
+        element_present = EC.presence_of_element_located((By.CLASS_NAME, 'staking-block'))
+        WebDriverWait(browser, timeout).until(element_present)
+        # get VRA text
+        vra_element = browser.find_elements(By.CSS_SELECTOR, '.staking-block .staking-block-value')[2].text
+        # Parse VRA from string to int
+        vra_number = int(vra_element.replace(' VRA', ''))
+        print(f'VRA actual staking status ${vra_number}')
+        if first_run is None:
+            print(f'First run')
+            first_run = False
+            vra_default_value = vra_number
+            print(f'VRA default value is ${vra_default_value}')
+        # is available for stake ( greater than 0 or different to previous value )
+        # available_for_stake = (vra_number > 0 and vra_number != vra_default_value)
+        available_for_stake = True
+        print(f'Is VRA available for stake or has changed ${available_for_stake}')
+        if available_for_stake:
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(email_data.gmail_user, email_data.gmail_password)
+            email_data.set_mail_body(vra_default_value, vra_number)
+            email_data.set_mail_text(email_data.body)
+            server.sendmail(email_data.gmail_user, email_data.to, email_data.text)
+            server.close()
+            print('Email Sent Correctly!')
 
-browser.quit()
+    except Exception as e:
+        logging.exception(e)
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+
+    browser.quit()
+
+
+main()
